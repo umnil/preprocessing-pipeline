c98020a64e5e4ad78bc2108b514eaccadf8966de
Kevin Davis <kevincarrolldavis@gmail.com>
2022-11-02 14:43:28 -0400

    Add wcs calculation function

---
+++ tests/pipeline/test_ext.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd  # type: ignore
+
+from pipeline.ext import calc_wcs
+
+
+class TestExt:
+    def test_calc_wcs(self):
+        pipeline_dir: str = os.path.dirname(__file__)
+        test_dir: str = os.path.join(pipeline_dir, "..")
+        data_dir: str = os.path.join(test_dir, "data")
+        data_filename: str = "sample.p"
+        data_filepath: str = os.path.join(data_dir, data_filename)
+
+        df: pd.DataFrame = pd.read_pickle(data_filepath)
+        result = calc_wcs(df)
+        assert result == [24880, 622] * 2
