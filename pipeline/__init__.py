8bf4a711e586674a5391ee9c0c3f7357f79618d3
Kevin Davis <kevincarrolldavis@gmail.com>
2023-02-20 19:42:23 -0500

    expose PSDBinner

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,17 +1,21 @@
 from .extractor import Extractor
-from .windower import Windower
-from .filter import Filterer
 from .featurizer import Featurizer
+from .filter import Filterer
+from .psdbinner import PSDBinner
 from .transform_pipeline import TransformPipeline
+from .windower import Windower
 
 Extractor
 Featurizer
 Filterer
+PSDBinner
 Windower
 
-preprocessor = TransformPipeline([
-    ("ex", Extractor()),
-    ("wn", Windower()),
-    ("fl", Filterer(filter_args={"sfreq": 200, "l_freq": 1, "h_freq": None})),
-    ("ft", Featurizer())
-])
+preprocessor = TransformPipeline(
+    [
+        ("ex", Extractor()),
+        ("wn", Windower()),
+        ("fl", Filterer(filter_args={"sfreq": 200, "l_freq": 1, "h_freq": None})),
+        ("ft", Featurizer()),
+    ]
+)
