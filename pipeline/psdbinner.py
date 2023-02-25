460c158c883432d11816338a970f6df2a595d292
Kevin Davis <kevincarrolldavis@gmail.com>
2023-02-25 17:31:40 -0500

    Custom function for PSDBinner

--- pipeline/psdbinner.py
+++ pipeline/psdbinner.py
@@ -1,14 +1,22 @@
 import numpy as np
 
 from sklearn.base import BaseEstimator, TransformerMixin  # type: ignore
-from typing import List, Tuple
+from typing import Callable, List, Tuple, Union, cast
 
 
 class PSDBinner(TransformerMixin, BaseEstimator):
-    def __init__(self, bins: List, sfreq: int, select_bins: List = []):
+    def __init__(
+        self,
+        bins: List,
+        sfreq: int,
+        select_bins: List = [],
+        fn: Union[Callable, str] = "mean",
+    ):
         self.bins: List = bins
+        self.fn: Union[Callable, str] = fn
         self.sfreq: int = sfreq
         self.select_bins: List = select_bins
+        self.freqs: Union[List, np.ndarray] = []
 
     def fit(self, *args, **kwargs) -> "PSDBinner":
         return self
@@ -18,12 +26,25 @@ class PSDBinner(TransformerMixin, BaseEstimator):
         output_shape: List = list(input_shape[:-1]) + [len(self.bins)]
         x = x.reshape(-1, input_shape[-1])
         nfreq: int = input_shape[-1]
-        freqs: np.ndarray = np.linspace(0, self.sfreq / 2, nfreq)
+        self.freqs = np.linspace(0, self.sfreq / 2, nfreq)
         freq_idxs: List = [
-            [np.where((freqs >= lo) & (freqs < h))[0] for lo, h in self.bins] for i in x
+            [np.where((self.freqs >= lo) & (self.freqs < h))[0] for lo, h in self.bins]
+            for i in x
         ]
-        x = np.array([[i[f].mean() for f in fi] for i, fi in zip(x, freq_idxs)])
+        x = np.array(
+            [
+                [self._fn(xi[f], i) for f in fi]
+                for i, (xi, fi) in enumerate(zip(x, freq_idxs))
+            ]
+        )
         x = x.reshape(output_shape)
         if len(self.select_bins) > 0:
             x = x[..., self.select_bins]
         return x
+
+    def _fn(self, x: np.ndarray, i: int) -> np.ndarray:
+        if type(self.fn) == str:
+            return np.__dict__[self.fn](x)
+        else:
+            f: Callable = cast(Callable, self.fn)
+            return f(x, i)
