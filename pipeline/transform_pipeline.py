b0a858bf8cfff3a8dd3fb080f8407afff6bb7f7a
Kevin Davis <kevincarrolldavis@gmail.com>
2023-04-24 19:23:21 -0400

    Implement score, fit_transform, and others

--- pipeline/transform_pipeline.py
+++ pipeline/transform_pipeline.py
@@ -4,10 +4,176 @@ import numpy as np
 from typing import Tuple, IO, Dict, Any, List
 from datetime import datetime
 
+from sklearn.base import clone  # type: ignore
 from sklearn.pipeline import Pipeline  # type: ignore
+from sklearn.utils import _print_elapsed_time  # type : ignore
+from sklearn.utils.metaestimators import available_if  # type : ignore
+from sklearn.utils.validation import check_memory  # type : ignore
+
+
+def _final_estimator_has(attr):
+    """Check that final_estimator has `attr`.
+    Used together with `avaliable_if` in `Pipeline`."""
+
+    def check(self):
+        # raise original `AttributeError` if `attr` does not exist
+        getattr(self._final_estimator, attr)
+        return True
+
+    return check
 
 
 class TransformPipeline(Pipeline):
+    def _fit(self, X: np.ndarray, y: np.ndarray = None, **fit_params_steps):
+        # shallow copy of steps - this should really be steps_
+        self.steps = list(self.steps)
+        self._validate_steps()
+        # Setup the memory
+        memory = check_memory(self.memory)
+
+        fit_transform_one_cached = memory.cache(_fit_transform_one)
+
+        for step_idx, name, transformer in self._iter(
+            with_final=False, filter_passthrough=False
+        ):
+            if transformer is None or transformer == "passthrough":
+                with _print_elapsed_time("Pipeline", self._log_message(step_idx)):
+                    continue
+
+            if hasattr(memory, "location") and memory.location is None:
+                # we do not clone when caching is disabled to
+                # preserve backward compatibility
+                cloned_transformer = transformer
+            else:
+                cloned_transformer = clone(transformer)
+            # Fit or load from cache the current transformer
+            X, y, fitted_transformer = fit_transform_one_cached(
+                cloned_transformer,
+                X,
+                y,
+                None,
+                message_clsname="Pipeline",
+                message=self._log_message(step_idx),
+                **fit_params_steps[name],
+            )
+            # Replace the transformer of the step with the fitted
+            # transformer. This is necessary when loading the transformer
+            # from the cache.
+            self.steps[step_idx] = (name, fitted_transformer)
+        return X, y
+
+    def fit(self, X: np.ndarray, y: np.ndarray = None, **fit_params):
+        """Fit the model.
+        Fit all the transformers one after the other and transform the
+        data. Finally, fit the transformed data using the final estimator.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            Training data. Must fulfill input requirements of first step of the
+            pipeline.
+        y : np.ndarray, default=None
+            Training targets. Must fulfill label requirements for all steps of
+            the pipeline.
+        **fit_params : dict of string -> object
+            Parameters passed to the ``fit`` method of each step, where
+            each parameter name is prefixed such that parameter ``p`` for step
+            ``s`` has key ``s__p``.
+
+        Returns
+        -------
+        self : object
+            Pipeline with fitted steps.
+        """
+        fit_params_steps = self._check_fit_params(**fit_params)
+        Xt: np.ndarray
+        yt: np.ndarray
+        Xt, yt = self._fit(X, y, **fit_params_steps)
+        with _print_elapsed_time("Pipeline", self._log_message(len(self.steps) - 1)):
+            if self._final_estimator != "passthrough":
+                fit_params_last_step = fit_params_steps[self.steps[-1][0]]
+                self._final_estimator.fit(Xt, yt, **fit_params_last_step)
+
+        return self
+
+    def fit_transform(self, X, y=None, **fit_params):
+        """Fit the model and transform with the final estimator.
+        Fits all the transformers one after the other and transform the
+        data. Then uses `fit_transform` on transformed data with the final
+        estimator.
+        Parameters
+        ----------
+        X : iterable
+            Training data. Must fulfill input requirements of first step of the
+            pipeline.
+        y : iterable, default=None
+            Training targets. Must fulfill label requirements for all steps of
+            the pipeline.
+        **fit_params : dict of string -> object
+            Parameters passed to the ``fit`` method of each step, where
+            each parameter name is prefixed such that parameter ``p`` for step
+            ``s`` has key ``s__p``.
+        Returns
+        -------
+        Xt : ndarray of shape (n_samples, n_transformed_features)
+            Transformed samples.
+        """
+        fit_params_steps = self._check_fit_params(**fit_params)
+        Xt, yt = self._fit(X, y, **fit_params_steps)
+
+        last_step = self._final_estimator
+        with _print_elapsed_time("Pipeline", self._log_message(len(self.steps) - 1)):
+            if last_step == "passthrough":
+                return Xt, yt
+            fit_params_last_step = fit_params_steps[self.steps[-1][0]]
+            if hasattr(last_step, "fit_transform"):
+                return last_step.fit_transform(Xt, yt, **fit_params_last_step)
+            else:
+                return last_step.fit(Xt, yt, **fit_params_last_step).transform(Xt)
+
+    @available_if(_final_estimator_has("score"))
+    def score(self, X, y=None, sample_weight=None):
+        """Transform the data, and apply `score` with the final estimator.
+        Call `transform` of each transformer in the pipeline. The transformed
+        data are finally passed to the final estimator that calls
+        `score` method. Only valid if the final estimator implements `score`.
+        Parameters
+        ----------
+        X : iterable
+            Data to predict on. Must fulfill input requirements of first step
+            of the pipeline.
+        y : iterable, default=None
+            Targets used for scoring. Must fulfill label requirements for all
+            steps of the pipeline.
+        sample_weight : array-like, default=None
+            If not None, this argument is passed as ``sample_weight`` keyword
+            argument to the ``score`` method of the final estimator.
+        Returns
+        -------
+        score : float
+            Result of calling `score` on the final estimator.
+        """
+        Xt = X
+        yt = y
+        for _, name, transform in self._iter(with_final=False):
+            if hasattr(transform, "_y_hat"):
+                Xt = transform.fit_transform(Xt, yt)
+            else:
+                Xt = transform.transform(Xt)
+            yt = (
+                yt if not hasattr(transform, "_y_hat") else getattr(transform, "_y_hat")
+            )
+            # Check for nan
+            if np.isnan(yt.astype(np.float64)).any():
+                nan_mask = np.isnan(yt)
+                Xt = Xt[~nan_mask]
+                yt = yt[~nan_mask]
+
+        score_params = {}
+        if sample_weight is not None:
+            score_params["sample_weight"] = sample_weight
+        return self.steps[-1][1].score(Xt, yt, **score_params)
+
     def transform(
         self, X: np.ndarray, y: np.ndarray, debug: bool = False
     ) -> Tuple[np.ndarray, np.ndarray]:
@@ -97,3 +263,38 @@ class TransformPipeline(Pipeline):
                     pickle.dump(data_artifact, fh)
 
             raise e
+
+
+def _fit_transform_one(
+    transformer, X, y, weight, message_clsname="", message=None, **fit_params
+):
+    """
+    Fits ``transformer`` to ``X`` and ``y``. The transformed result is returned
+    with the fitted transformer. If ``weight`` is not ``None``, the result will
+    be multiplied by ``weight``.
+    """
+    with _print_elapsed_time(message_clsname, message):
+        if hasattr(transformer, "fit_transform"):
+            res_x = transformer.fit_transform(X, y, **fit_params)
+            res_y = (
+                y
+                if not hasattr(transformer, "_y_hat")
+                else getattr(transformer, "_y_hat")
+            )
+        else:
+            res_x = transformer.fit(X, y, **fit_params).transform(X)
+            res_y = (
+                y
+                if not hasattr(transformer, "_y_hat")
+                else getattr(transformer, "_y_hat")
+            )
+
+        # Check for nan
+        if np.isnan(res_y.astype(np.float64)).any():
+            nan_mask = np.isnan(res_y)
+            res_x = res_x[~nan_mask]
+            res_y = res_y[~nan_mask]
+
+    if weight is None:
+        return res_x, res_y, transformer
+    return res_x * weight, res_y, transformer
