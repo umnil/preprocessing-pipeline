daa98cb1401254e6fbf1f14d1d0a34ac5095e3cf
Kevin Davis <kevincarrolldavis@gmail.com>
2023-05-12 11:49:50 -0400

    Mypy

--- pipeline/feature_union.py
+++ pipeline/feature_union.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from sklearn.pipeline import FeatureUnion
-from sklearn.utils.parallel import Parallel, delayed
+from sklearn.pipeline import FeatureUnion  # type: ignore
+from sklearn.utils.parallel import Parallel, delayed  # type: ignore
 from .transform_pipeline import _transform_one
 
 
