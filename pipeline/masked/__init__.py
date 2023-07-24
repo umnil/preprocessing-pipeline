163e62e087af5e8a65a62a8e7bef7e179f874b4c
ahmad1111 <ata83@miami.edu>
2023-07-24 18:42:17 -0400

    added self.verbose = None to the file

--- pipeline/masked/__init__.py
+++ pipeline/masked/__init__.py
@@ -3,7 +3,7 @@ from .polydetrend import MaskedPolyDetrend as PolyDetrend
 from .psdbinner import MaskedPSDBinner as PSDBinner
 from .psdestimator import MaskedPSDEstimator as PSDEstimator
 from .temporal_filter import MaskedTemporalFilter as TemporalFilter
-
+self.verbose = None
 Masker
 PolyDetrend
 PSDBinner
