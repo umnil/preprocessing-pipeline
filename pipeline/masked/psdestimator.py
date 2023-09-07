4164b6965f57e05e61db2b2b0a6a6c41e7b28153
Kevin Davis <kevincarrolldavis@gmail.com>
2023-09-06 22:30:44 -0400

    Demasking is skipped if not needed and defaults are updated

--- pipeline/masked/psdestimator.py
+++ pipeline/masked/psdestimator.py
@@ -8,15 +8,16 @@ class MaskedPSDEstimator(PSDEstimator):
     # super().__init__()
     def __init__(
         self,
-        sfreq=None,
-        fmin=None,
-        fmax=None,
+        sfreq=2 * np.pi,
+        fmin=0,
+        fmax=np.inf,
         bandwidth=None,
         adaptive=False,
         low_bias=True,
-        n_jobs=4,
-        normalization=None,
+        n_jobs=None,
+        normalization="length",
         *,
+        demask: bool = True,
         verbose=None
     ):
         self.verbose = None
@@ -31,17 +32,46 @@ class MaskedPSDEstimator(PSDEstimator):
             normalization,
             verbose=None,
         )
+        self.demask: bool = demask
 
     def transform(self, x: np.ma.core.MaskedArray, **kwargs) -> np.ma.core.MaskedArray:
+        """Transform the time series data into the frequency domain
+        using power spectral density
+
+        Parameters
+        ----------
+        x : np.ma.core.MaskedArray
+            The input matrix. Transformed axis is the last
+        """
+        if not isinstance(x, np.ma.core.MaskedArray):
+            return super(MaskedPSDEstimator, self).transform(x, **kwargs)
+
+        if not self.demask:
+            input_mask: np.ndarray = x.mask.copy()
+            x = super(MaskedPSDEstimator, self).transform(x, **kwargs)
+            output_mask: np.ndarray = input_mask[..., : x.shape[-1]]
+            x = np.ma.MaskedArray(x, output_mask)
+            return x
+
         input_shape: Tuple = x.shape
         t: int = input_shape[-1]
         df: int = int(t / 2 + 1)
         output_shape: List = list(input_shape[:-1]) + [df]
         x = x.reshape(-1, t)
-        list_x: List = [
-            super(MaskedPSDEstimator, self).transform(i[~i.mask]) for i in x
-        ]
-        np_x = np.array([np.concatenate([i, [np.nan] * (df - i.size)]) for i in list_x])
+
+        # unmaks x
+        list_x: List = [i.data[~i.mask] for i in x]
+
+        # transform
+        window_lengths: np.ndarray = np.unique([i.size for i in list_x if i.size != 0])
+        if len(window_lengths) == 1:
+            np_x = super(MaskedPSDEstimator, self).transform(x)
+        else:
+            list_x = [super(MaskedPSDEstimator, self).transform(i[~i.mask]) for i in x]
+            np_x = np.array(
+                [np.concatenate([i, [np.nan] * (df - i.size)]) for i in list_x]
+            )
+
         x = np.ma.masked_invalid(np_x)
         x = x.reshape(output_shape)
         return x
