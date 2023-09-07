fbcad98de7724d82ad1cf009afd67c20620fa495
Kevin Davis <kevincarrolldavis@gmail.com>
2023-09-06 22:28:32 -0400

    Adjust masking for separate fit and transforms

--- pipeline/masked/mask.py
+++ pipeline/masked/mask.py
@@ -7,14 +7,24 @@ class Masker(TransformerMixin, BaseEstimator):
     def __init__(self, filler=None):
         self.filler = filler
 
-    def fit(self, *args, **kwargs):
+    def fit(self, *args, **fit_params):
         return self
 
-    def transform(self, x, *args, **kwargs):
+    def fit_transform(self, x, y=None, **fit_params):
+        return self.transform(x, y, **fit_params)
+
+    def transform(self, x, y=None, *args, **kwargs):
         if self.filler is None:
             ret = np.ma.masked_invalid(x)
             ret.data[ret.mask] = 0
+            if y is not None:
+                self._y_hat = np.ma.masked_invalid(y)
         else:
             ret = np.ma.masked_values(x, self.filler)
+            if y is not None:
+                self._y_hat = np.ma.masked_values(y, self.filler)
+
         ret.harden_mask()
+        if y is not None:
+            self._y_hat.harden_mask()
         return ret
