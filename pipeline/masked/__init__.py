69d7c12768664d525a2bc8bae00cda3314227d18
Kevin Davis <kevincarrolldavis@gmail.com>
2023-09-08 00:09:46 -0400

    Add roc_auc metrics

--- pipeline/masked/__init__.py
+++ pipeline/masked/__init__.py
@@ -1,14 +1,22 @@
 from .mask import Masker
-from .metrics import masked_accuracy_score as accuracy_score
+from .metrics import (
+    masked_accuracy_score as accuracy_score,
+    masked_roc_auc_score as roc_auc_score,
+)
+from .minmax_scaler import MaskedMinMaxScaler as MinMaxScaler
 from .polydetrend import MaskedPolyDetrend as PolyDetrend
 from .psdbinner import MaskedPSDBinner as PSDBinner
 from .psdestimator import MaskedPSDEstimator as PSDEstimator
+from .standard_scaler import MaskedStandardScaler as StandardScaler
 from .temporal_filter import MaskedTemporalFilter as TemporalFilter
 
 Masker
+MinMaxScaler
 PolyDetrend
 PSDBinner
 PSDEstimator
+StandardScaler
 TemporalFilter
 
 accuracy_score
+roc_auc_score
--- pipeline/masked/metrics.py
+++ pipeline/masked/metrics.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.metrics import accuracy_score  # type: ignore
+from sklearn.metrics import accuracy_score, roc_auc_score  # type: ignore
 
 
 def masked_accuracy_score(
@@ -11,3 +11,14 @@ def masked_accuracy_score(
     y_pred = y_pred[~y_true.flatten().mask]
     y_true = y_true.data[~y_true.mask]
     return accuracy_score(y_true, y_pred, *args, **kwargs)
+
+
+def masked_roc_auc_score(
+    y_true: np.ma.core.MaskedArray, y_pred: np.ma.core.MaskedArray, *args, **kwargs
+) -> float:
+    if not isinstance(y_true, np.ma.core.MaskedArray):
+        return roc_auc_score(y_true, y_pred)
+
+    y_pred = y_pred[~y_true.flatten().mask]
+    y_true = y_true.data[~y_true.mask]
+    return roc_auc_score(y_true, y_pred, *args, **kwargs)
